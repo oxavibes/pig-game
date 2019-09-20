18d69f194964ebecb53a7cbf5c37d01b9b1aea2d
Stefano <bozzistef@gmail.com>
2019-09-20 14:29:08 -0400

    Game finished

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ GAME RULES:
 */
 
 /********************************
-***** GAME INITIALIZATION ******
-********************************/
+ ***** GAME INITIALIZATION ******
+ ********************************/
 
 var playerOne = {
     score: 0,
     currentScore: 0,
-    holdScore: function(){
+    holdScore: function () {
         this.score += this.currentScore;
         this.currentScore = 0;
     },
-    addCurrentScore: function(value){
+    addCurrentScore: function (value) {
         this.currentScore += value;
     },
-    reset: function(){
+    reset: function () {
         this.score = 0;
         this.currentScore = 0;
     }
@@ -33,14 +33,14 @@ var playerOne = {
 var playerTwo = {
     score: 0,
     currentScore: 0,
-    holdScore: function(){
+    holdScore: function () {
         this.score += this.currentScore;
         this.currentScore = 0;
     },
-    addCurrentScore: function(value){
+    addCurrentScore: function (value) {
         this.currentScore += value;
     },
-    reset: function(){
+    reset: function () {
         this.score = 0;
         this.currentScore = 0;
     }
@@ -48,30 +48,38 @@ var playerTwo = {
 
 
 var activePlayer = 0;
-
+var MAX_SCORE = 50;
 
 /********************************
-********** SELECTORS ************
-********************************/
+ ********** SELECTORS ************
+ ********************************/
+
+//Players name
+var $playerOneName = document.querySelector('#name-0');
+var $playerTwoName = document.querySelector('#name-1');
 
 //Players score
 var scorePlayerOne = document.querySelector('#score-0');
 var scorePlayerTwo = document.querySelector('#score-1');
 
-var currentScorePlayerOne = document.querySelector('#current-0');
-var currentScorePlayerTwo = document.querySelector('#current-1');
+var $currentScorePlayerOne = document.querySelector('#current-0');
+var $currentScorePlayerTwo = document.querySelector('#current-1');
+
+//Players Panel
+var $playerOnePanel = document.querySelector('.player-0-panel');
+var $playerTwoPanel = document.querySelector('.player-1-panel');
 
 //Buttons
-var newGameButton = document.querySelector('.btn-new');
-var rollDiceButton = document.querySelector('.btn-roll');
-var holdPointsButton = document.querySelector('.btn-hold');
+var $newGameButton = document.querySelector('.btn-new');
+var $rollDiceButton = document.querySelector('.btn-roll');
+var $holdPointsButton = document.querySelector('.btn-hold');
 
 // Dice
-var dice =  document.querySelector('.dice');
+var $dice = document.querySelector('.dice');
 
 /********************************
-******** GAME FUNCTIONS ********
-********************************/
+ ******** GAME FUNCTIONS ********
+ ********************************/
 
 function initGame() {
     newGame();
@@ -83,22 +91,31 @@ function newGame() {
     playerOne.reset();
     playerTwo.reset();
 
+    $playerOneName.textContent = 'Player 1';
+    $playerTwoName.textContent = 'Player 2';
+
     scorePlayerOne.textContent = 0;
     scorePlayerTwo.textContent = 0;
 
-    currentScorePlayerOne.textContent = 0;
-    currentScorePlayerTwo.textContent = 0;
+    $currentScorePlayerOne.textContent = 0;
+    $currentScorePlayerTwo.textContent = 0;
 
-    document.querySelector('.player-0-panel').classList.add('active');
-    document.querySelector('.player-1-panel').classList.remove('active');
+    $playerOnePanel.classList.add('active');
+    $playerTwoPanel.classList.remove('active');
 
-    dice.classList.add('d-none');
+    $playerOnePanel.classList.remove('winner');
+    $playerTwoPanel.classList.remove('winner');
+
+    $rollDiceButton.removeAttribute('disabled');
+    $holdPointsButton.removeAttribute('disabled');
+
+    $dice.classList.add('d-none');
 }
 
 function switchPlayer() {
     //Resetting the current player score
     document.querySelector('#current-' + activePlayer).textContent = 0;
-    
+
     //Removing active class from the current player
     document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 
@@ -113,68 +130,88 @@ function rollDice() {
     //Get random number between 1 and 6
     var number = getRandomNumber(1, 6);
 
-
-    if(number === 1) {
+    if (number === 1) {
         //Switching to the other player
         switchPlayer();
 
         //Resetting the current player score
-        if(activePlayer === 0) {
+        if (activePlayer === 0) {
             playerOne.currentScore = 0;
         } else {
             playerTwo.currentScore = 0;
         }
 
+        $dice.classList.add('d-none');
 
-        dice.classList.add('d-none');
         return;
     }
-        
 
     //Setting dice based on the random number
-    dice.setAttribute('src', 'dice-' + number + '.png');
+    $dice.src = 'dice-' + number + '.png';
 
     //Adding points to the current active player
-    if(activePlayer === 0){
+    if (activePlayer === 0) {
         playerOne.addCurrentScore(number);
-        currentScorePlayerOne.textContent = playerOne.currentScore;
+        $currentScorePlayerOne.textContent = playerOne.currentScore;
     } else {
         playerTwo.addCurrentScore(number);
-        currentScorePlayerTwo.textContent = playerTwo.currentScore;
-    } 
+        $currentScorePlayerTwo.textContent = playerTwo.currentScore;
+    }
 
-    //Validating if the class has been already removed    
-    if(dice.classList.contains('d-none'))
-        dice.classList.remove('d-none');
+    $dice.classList.remove('d-none');
 }
 
 function holdPoints() {
-    if(activePlayer === 0) {
+    if (activePlayer === 0) {
         playerOne.holdScore();
         scorePlayerOne.textContent = playerOne.score;
-        currentScorePlayerOne.textContent = 0;
+        $currentScorePlayerOne.textContent = 0;
     } else {
         playerTwo.holdScore();
         scorePlayerTwo.textContent = playerTwo.score;
-        currentScorePlayerTwo.textContent = 0;
+        $currentScorePlayerTwo.textContent = 0;
     }
+
+    if (thereIsWinner()) return;
 
     switchPlayer();
 }
 
+function thereIsWinner() {
+    if (playerOne.score >= MAX_SCORE) {
+        $playerOneName.textContent = 'Winner!';
+
+        $playerOnePanel.classList.add('winner');
+
+        $rollDiceButton.setAttribute('disabled', true);
+        $holdPointsButton.setAttribute('disabled', true);
+
+        return true;
+
+    } else if (playerTwo.score >= MAX_SCORE) {
+        $playerTwoName.textContent = 'Winner!';
+
+        $playerTwoPanel.classList.add('winner');
+        $rollDiceButton.setAttribute('disabled', true);
+        $holdPointsButton.setAttribute('disabled', true);
+
+        return true;
+    }
+    return false;
+}
+
 function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min; 
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
 
 /********************************
-******** GAME EVENTS ************
-********************************/
+ ******** GAME EVENTS ************
+ ********************************/
 
-newGameButton.addEventListener('click', newGame);
-rollDiceButton.addEventListener('click', rollDice);
-holdPointsButton.addEventListener('click', holdPoints);
+$newGameButton.addEventListener('click', newGame);
+$rollDiceButton.addEventListener('click', rollDice);
+$holdPointsButton.addEventListener('click', holdPoints);
 
 
 initGame();
-
